b81f60673fa1af534994b992f6f0465b28b30a90
joddiy <joddiy@qq.com>
2018-08-23 08:50:17 +0800

    commit

--- views/site/index.php
+++ views/site/index.php
@@ -39,9 +39,8 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="object-non-visible" data-animation-effect="fadeIn" data-effect-delay="100">
                         <!-- logo -->
                         <div id="logo" class="logo text-center">
-                            <a href="/soc" style="width: 150px; display: flex; margin: auto"><img src="/soc/img/NUS-SOC-Logo-1.png"
-                                                                                               style="height: 50px; width: 150px"
-                                                                                               alt="The Project"></a>
+                            <h3 class="margin-clear"><a href="/" class="logo-font link-light"><span
+                                            class="text-default">SoC</span></a></h3>
                         </div>
                         <p class="small"></p>
                         <!-- name-and-slogan -->
